e9e8814f78b078489039195a3800a468ca1d8447
positoy <positoy@gmail.com>
2020-08-08 21:31:16 +0900

    Initial commit - import from start.spring.io - dependecies including (spring-boot-web, lombok, mysql, h2, thymeleaf)

---
+++ src/main/java/io/github/positoy/oauthaccountboard/OauthAccountBoardApplication.java
@@ -0,0 +1,13 @@
+package io.github.positoy.oauthaccountboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAccountBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthAccountBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/positoy/oauthaccountboard/OauthAccountBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.positoy.oauthaccountboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAccountBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
