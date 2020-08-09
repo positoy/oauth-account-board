3a4070472fd43a5d20146e7fe3dd9752b14ceaa2
positoy <positoy@gmail.com>
2020-08-09 12:00:34 +0900

    add LoginService to handle session and accountId

--- src/main/java/io/github/positoy/oauthaccountboard/BoardController.java
+++ src/main/java/io/github/positoy/oauthaccountboard/BoardController.java
@@ -2,6 +2,7 @@ package io.github.positoy.oauthaccountboard;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +11,14 @@ public class BoardController {
 
     final static Logger logger = LoggerFactory.getLogger(BoardController.class);
 
+    @Autowired
+    LoginService loginService;
+
     @GetMapping("/login")
     public String loginNaver(@RequestParam(defaultValue="") String code) {
         logger.info("code : " + code);
+        int accountId = loginService.getAccountId(ResourceServer.NAVER, code);
+        logger.info("accountId : " + accountId);
         return "redirect:/board";
     }
 
--- src/main/java/io/github/positoy/oauthaccountboard/LoginService.java
+++ src/main/java/io/github/positoy/oauthaccountboard/LoginService.java
@@ -1,4 +1,18 @@
 package io.github.positoy.oauthaccountboard;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
+
+    final static Logger logger = LoggerFactory.getLogger(LoginService.class);
+
+    public int getAccountId(ResourceServer resourceServer, String authcode) {
+        logger.info("serviceName : " + resourceServer.getServiceName() + ", authcode : " + authcode);
+
+        return 0;
+    }
 }
